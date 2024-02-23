e3d19b651b57ef2907e62059619480684aca3532
TruongDuyKhai <102969311+TruongDuyKhai@users.noreply.github.com>
2024-02-23 11:29:50 +0700

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,12 +10,11 @@ import styles from './index.module.css';
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   async function download() {
+    if (typeof window == 'undefined') return;
     if (!navigator) return;
     const permissionStatus = await navigator.permissions?.query({ name: 'clipboard-read' });
     if (!permissionStatus || permissionStatus?.state === 'denied') {
-      if (typeof window !== 'undefined')
-        return alert('Permission to read from the clipboard is denied');
-      return console.error('Permission to read from the clipboard is denied')
+      return alert('Permission to read from the clipboard is denied');
     }
     navigator.clipboard.writeText('npm i @khaidev1012/funcs');
   }
