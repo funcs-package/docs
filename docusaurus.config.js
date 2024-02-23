c96cc7ea263bfc27b2fa27039c69ff3efa46a3ae
TruongDuyKhai <102969311+TruongDuyKhai@users.noreply.github.com>
2024-02-23 11:13:11 +0700

    .

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   tagline: 'A Package that synthesis of utility functions',
 
   // Set the production url of your site here
-  url: 'https://funcs-package.github.io/docs',
+  url: 'https://funcs-package.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs/',
 
   deploymentBranch: 'deployment',
 
